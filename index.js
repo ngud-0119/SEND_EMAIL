dd84afe4f6130786d12f9869b7624655537b8316
ngud-0119 <finaldev.dream119@gmail.com>
2024-01-05 04:34:07 -0500

    using mailgen

--- index.js
+++ index.js
@@ -1,48 +1,64 @@
 // filename: index.js
+// Creating an endpoint for sending customized emails
+app.post('/custom-email',(req, res)=>{
+  let config = {
+      service: 'gmail',
+      auth: {
+          user: process.env.NODEJS_GMAIL_APP_USER,
+          pass: process.env.NODEJS_GMAIL_APP_PASSWORD
+      }
+  }
+  let transporter = nodemailer.createTransport(config);
 
-const express = require('express');
-const nodemailer = require('nodemailer');
-const Mailgen = require('mailgen');
-require('dotenv').config();
-const app = express();
-const port = 3001;
+  let MailGenerator = new Mailgen({
+      theme: 'default',
+      product: {
+          name: 'YOUR_PRODUCT_NAME',
+          link: 'https://mailgen.js/'
+      }
+  });
 
-app.use(express.json());
+  let response = {
+      body: {
+          name: 'Name',
+          intro: 'Welcome to ABC Company! We\'re very excited to have you on board.',
+          action: {
+              instructions: 'To get started with ABC, please click here:',
+              button: {
+                  color: '#22BC66', // Optional action button color
+                  text: 'Confirm your account',
+                  link: 'https://mailgen.js/'
+              }
+          }
+      }
+  };
 
-app.post('/email',(req, res)=>{
-    let config = {
-        service: 'gmail', // your email domain
-        auth: {
-            user: process.env.NODEJS_GMAIL_APP_USER,   // your email address
-            pass: process.env.NODEJS_GMAIL_APP_PASSWORD // your password
-        }
-    }
-    let transporter = nodemailer.createTransport(config);
+  let mail = MailGenerator.generate(response);
 
-    let message = {
-        from: 'ibrahimhz@gmail.com', // sender address
-        to: req.body.email, // list of receivers
-        subject: 'Welcome to ABC Website!', // Subject line
-        html: "<b>Hello world?</b>", // html body
-        attachments: [ // use URL as an attachment
-            {
-              filename: 'cat image',
-              path: '<link of the document>',
-              cid: 'unique cat image id' 
-            }
-        ]
-    };
+  let message = {
+      from: 'ibrahimhz@gmail.com',
+      to: req.body.email,
+      subject: 'Welcome to ABC company!',
+      html: mail,
+      attachments: [
+          {
+            filename: 'cat image',
+            path: '<link of the document>',
+            cid: 'unique cat image id'  
+          }
+      ]
+  };
 
-    transporter.sendMail(message).then((info) => {
-        return res.status(201).json(
-            {
-                msg: "Email sent",
-                info: info.messageId,
-                preview: nodemailer.getTestMessageUrl(info)
-            }
-        )
-    }).catch((err) => {
-        return res.status(500).json({ msg: err });
-    }
-    );
+  transporter.sendMail(message).then((info) => {
+      return res.status(201).json(
+          {
+              msg: "Email sent",
+              info: info.messageId,
+              preview: nodemailer.getTestMessageUrl(info)
+          }
+      )
+  }).catch((err) => {
+      return res.status(500).json({ msg: err });
+  }
+  );
 })
